ef6774141905d8053746a4e0643c18fd4c8dd8af
Kartikey1101 <143342564+Kartikey1101@users.noreply.github.com>
2024-05-15 18:17:04 +0000

    updated

--- generate_data.py
+++ generate_data.py
@@ -1,12 +1,12 @@
 import numpy as np 
 
-n = 1000
+n = 100
 d = 100
 c = 5
 ep = 3
 eta = d + 1 + c + ep
 
-data_to_save = np.random.randint(0,100, size=(n, d))
+data_to_save = np.random.randint(0,10000, size=(n, d))
 
 # Save data to a CSV file
 np.savetxt('data_user/data_og.csv', data_to_save, delimiter=',')
